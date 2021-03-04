07c9d775be77fa043cb053f49b6acdffa7646bf5
IamThiago-IT <thiagodossantos315@gmail.com>
2021-03-03 22:19:23 -0300

    Ap

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,1 +1,10 @@
 
+import { Router } from 'express';
+import UserCrontroller from './controllers/UserCrontroller';
+
+const routes = Router();
+
+routes.get('/users', UserCrontroller.index);
+routes.post('/users', UserCrontroller.create);
+
+export default routes;
